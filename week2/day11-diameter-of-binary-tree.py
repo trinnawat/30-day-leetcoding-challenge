01de3191f5e245ce9072bb442609592e06385336
Got <trinnawatgot@gmail.com>
2020-04-12 16:14:29 +0700

    change file name day11, solution for day12

--- week2/day11-diameter-of-binary-tree.py
+++ week2/day11-diameter-of-binary-tree.py
@@ -1,9 +1,14 @@
+'''
+    https://leetcode.com/problems/diameter-of-binary-tree/
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
 
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
---
+++ week2/day12-last-stone-weight.py
@@ -0,0 +1,23 @@
+import heapq
+'''
+    https://leetcode.com/problems/last-stone-weight/
+'''
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-x for x in stones]
+        heapq.heapify(stones)
+        while True:
+            if len(stones) < 2:
+                break
+            first_value = -heapq.heappop(stones)
+            next_value = -heapq.heappop(stones)
+            diff_value = first_value - next_value
+            if diff_value != 0:
+                heapq.heappush(stones, -diff_value)
+
+        if len(stones) == 1:
+            return -stones[0]
+        else:
+            return 0
