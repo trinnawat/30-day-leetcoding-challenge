41d8704b1a6747d41880dc8919ee97602d008fc3
Got <trinnawat_got@hotmail.com>
2020-04-26 23:16:44 +0700

    solution day26

---
+++ week3/day26-longest-common-subsequence.py
@@ -0,0 +1,18 @@
+'''
+    https://leetcode.com/problems/longest-common-subsequence
+
+    using dynamic programming
+'''
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        L = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    L[i][j] = L[i-1][j-1] + 1
+                else:
+                    L[i][j] = max(L[i-1][j], L[i][j-1])
+        return L[-1][-1]
